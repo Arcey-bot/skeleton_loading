3bcc8a9288caae4488d4e2c32c1d4f21988dc470
Vinny <quinterovinny@gmail.com>
2021-10-01 11:01:49 -0400

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/skeleton_loading_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.skeleton_loading_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widget/image_showcase.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'package:skeleton_loading_sample/util/image_requester.dart';
+
+class ImageShowcase extends StatelessWidget {
+  const ImageShowcase({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(),
+    );
+  }
+}
